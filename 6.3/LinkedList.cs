64ef8e251af88749d00dc664a00a2a89de8329c9
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2022-06-01 21:54:02 +0200

    Добавьте файлы проекта.

---
+++ 6.3/LinkedList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+                head = node;
+            else
+                tail.Next = node;
+            tail = node;
+            count++;
+        }
+        public Node<T> GetNode(LinkedList<T> list, int index)
+        {
+            count = 0;
+            Node<T> current = head;
+            for (int l = 0; l < index; l++)
+            {
+                current = current.Next;
+                count++;
+            }
+            return current;
+        }
+        public string GetNodeString(Node<T> current)
+        {
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+            Console.SetError(sw);
+            if (current != null)
+            {
+                Console.WriteLine(current.Data);
+                return sw.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public void Remove(T Data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+            while (current != null)
+            {
+                if (current.Data.Equals(Data))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        head = head.Next;
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                }
+                previous = current;
+                current = current.Next;
+            }
+        }
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public LinkedList<T> Next { get; private set; }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
---
+++ 6.3/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6._3
+{
+
+    //Гребенюк А, 21-ИСП-2, 7-я лаборатоная, 2-й вариант, средний уровень.
+    public partial class MainWindow : Window
+    {
+        public int linkedcount;
+        public LinkedList<double> list;
+        public string node;
+        public MainWindow()
+        {
+            InitializeComponent();
+            list = new LinkedList<double>();
+            linkedcount = list.Count;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list.Add(double.Parse(InputValue.Text));
+            Result.Items.Add(InputValue.Text);
+            InputValue.Clear();
+            linkedcount++;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            list.Remove(double.Parse(Result.SelectedItem.ToString()));
+            Result.Items.Remove(Result.SelectedItem.ToString());
+            linkedcount--;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < linkedcount; i++)
+            {
+                if (double.Parse(list.GetNodeString(list.GetNode(list, i))) < 0)
+                {
+                    list.Remove(Convert.ToDouble(list.GetNodeString(list.GetNode(list, i+1))));
+                    linkedcount--;
+                    Result.Items.Clear();
+                    for (int c = 0; c < linkedcount; c++)
+                    {
+                        Result.Items.Add(list.GetNodeString(list.GetNode(list, c)));
+                    }
+                }
+            }
+        }
+    }
+}
